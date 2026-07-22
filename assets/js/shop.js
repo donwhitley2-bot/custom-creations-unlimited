8a8c60caea143caa2aae20df114b0c4223c40c28
Don Whitley <donwhitley2@gmail.com>
2026-07-21 20:48:36 -0400

    Wire haec-tumbler Stripe link (instant-buy)

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -20,7 +20,8 @@ const SHOP_CONFIG = {
     "crystal-award": "",
     "slate-coasters": "",
     "tequila-tumbler": "https://buy.stripe.com/6oUbJ188Rd3U3zh4jLfrW01",
-    "drink-straight-tumbler": "https://buy.stripe.com/3cI28r60J7JAgm3dUlfrW02"
+    "drink-straight-tumbler": "https://buy.stripe.com/3cI28r60J7JAgm3dUlfrW02",
+    "haec-tumbler": "https://buy.stripe.com/fZu28r2Ox8NE9XF17zfrW03"
   },
 
   /* Where the customization details + uploaded artwork are sent when a
